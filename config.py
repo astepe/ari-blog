03cdde5471c6420ea666146432c9243fb51c3db4
Aris Stepe <arisstepe@gmail.com>
2018-10-11 21:33:37 -0400

    Changed database URL to correct environment variable DATABASE_URL

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or \
         '-av\x89\xe2\xf8\xab\xc3\xec\x0f\x81>B\xcf\x91\x0c<{\x16\xd2\xbe\x16%N'
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS') or False
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or '/static/pictures/'
