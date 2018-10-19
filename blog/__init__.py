770b34b969f6a1bec0e392008bb12dbdc1262f89
Aris Stepe <arisstepe@gmail.com>
2018-10-19 12:45:47 -0400

    postgres db

--- blog/__init__.py
+++ blog/__init__.py
@@ -11,7 +11,6 @@ def create_app(config_class=Config):
 
     app = Flask(__name__)
     app.config.from_object(Config)
-    app.config['DEBUG'] = True
 
     db.init_app(app)
     migrate.init_app(app, db)
--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or \
         '-av\x89\xe2\xf8\xab\xc3\xec\x0f\x81>B\xcf\x91\x0c<{\x16\xd2\xbe\x16%N'
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS') or False
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or '/static/images/'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID') or 'AKIAIEUTP7VATUFFQ6VA'
