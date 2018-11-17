ef32c9c5119c9de10f8a2e694d345a0eb160c52e
Aris Stepe <arisstepe@gmail.com>
2018-11-17 16:53:18 -0500

    added blog post summary to database

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,7 @@ class BlogPost(db.Model):
     title = db.Column(db.String(50), nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     body = db.Column(db.Text, nullable=False)
+    summary = db.Column(db.String(100), nullable=False, default='summary')
     image = db.Column(db.String(100), nullable=False, default='https://s3.us-east-2.amazonaws.com/ari-blog-assets/python.png')
 
     def __repr__(self):
