1d5cd4564525e0a6fc3b0061d304be9923efb8aa
Aris Stepe <arisstepe@gmail.com>
2018-12-13 20:41:46 -0500

    tweaked regexes

--- blog/projects/parser.py
+++ blog/projects/parser.py
@@ -9,15 +9,15 @@ class ChemicalData:
 
     CATEGORIES = (
         CATEGORY('Product Name', re.compile(r"[P|p]roduct (([N|n]ame)|([D|d]escription)).*?(?P<data>[^ :\n].*?)(P|C)", re.DOTALL)),
-        CATEGORY('Flash Point', re.compile(r"[F|f]lash [P|p]oint[^a-bd-zA-BD-Z]*?(?P<data>[0-9][^C]*?F)", re.DOTALL)),
-        CATEGORY('Specific Gravity', re.compile(r"([R|r]elative [D|d]ensity|[S|s]pecific [G|g]ravity)\s*?(?P<data>[0-9.]+?\s)", re.DOTALL)),
-        CATEGORY('CAS #', re.compile(r"(?P<data>\d{2,7}\n*?-\n*?\d{2}\n*?-\n*?\d)", re.DOTALL)),
+        CATEGORY('Flash Point', re.compile(r"([F|f]lash [P|p]oint)([\s:\d.°C]{0,18}\s{0,8}\(\s{0,4})?(?P<data>[0-9.]*\s*°?\s?F)?", re.DOTALL)),
+        CATEGORY('Specific Gravity', re.compile(r"([R|r]elative [D|d]ensity|[S|s]pecific [G|g]ravity)\s*(?P<data>[0-9.]*\s)?", re.DOTALL)),
+        CATEGORY('CAS #', re.compile(r"CAS.*?(?P<data>\d{2,7}\n*?-\n*?\d{2}\n*?-\n*?\d)", re.DOTALL)),
         CATEGORY('NFPA Fire', re.compile(r"NFPA.*?[F|f]ire.*?(?P<data>[0-4])", re.DOTALL)),
         CATEGORY('NFPA Health', re.compile(r"NFPA.*?[H|h]ealth.*?(?P<data>[0-4])", re.DOTALL)),
         CATEGORY('NFPA Reactivity', re.compile(r"NFPA.*?(([R|r]eactivity)|([I|i]nstability)).*?(?P<data>[0-4])", re.DOTALL)),
-        CATEGORY('SARA 311/312', re.compile(r"SARA 311.*?(?P<data>(Chronic|Acute|Fire|Reactive)(.{0,25}(Hazard|Reactive))+)", re.DOTALL)),
+        CATEGORY('SARA 311/312', re.compile(r"SARA 311.*?(?P<data>(Chronic|Acute|Fire|Reactive)+?(.{0,25}(Hazard|Reactive))+)", re.DOTALL)),
         CATEGORY('Revision Date', re.compile(r"[R|r]evision [D|d]ate.*?(?P<data>\d.*?\s)", re.DOTALL)),
-        CATEGORY('Physical State', re.compile(r"Form\W(.)*?(?P<data>\w+)\s", re.DOTALL))
+        CATEGORY('Physical State', re.compile(r"F[\s]*?o[\s]*?r[\s]*?m[\s]*?\W(.)*?(?P<data>\w+)\s", re.DOTALL))
         )
 
 
@@ -32,7 +32,7 @@ def sds_parser(sds_file):
 
 def get_chemical_data(text):
 
-    chemical_data = []
+    chemical_data = {}
 
     for category in ChemicalData.CATEGORIES:
 
@@ -40,13 +40,16 @@ def get_chemical_data(text):
 
         if match_found:
 
-            match = match_found.group('data').replace('\n', '')
-            chemical_data.append(match)
+            if match_found.group('data'):
+                match = match_found.group('data').replace('\n', '')
+            else:
+                match = 'Not Listed'
+            chemical_data[category.name] = match
             print(category.name + ': ' + match)
 
         else:
 
-            chemical_data.append('Not Found')
+            chemical_data[category.name] = 'Not Found'
             print(category.name + ': ' + 'Not Found')
 
     return chemical_data
