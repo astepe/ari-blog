6b1dae9c6a3cf349b2c1618c7e087257f12edbb6
Aris Stepe <arisstepe@gmail.com>
2019-01-27 21:36:11 -0500

    added basic fade in/out funtions

--- blog/static/myscripts.js
+++ blog/static/myscripts.js
@@ -1,3 +1,19 @@
+
+function fadeOutEffect(e) {
+  var fadeTarget = document.getElementsByClassName("fade");
+  var fadeEffect = setInterval(function () {
+    if (!fadeTarget.style.opacity) {
+        fadeTarget.style.opacity = 1;
+    }
+    if (fadeTarget.style.opacity > 0) {
+        fadeTarget.style.opacity -= 0.1;
+    } else {
+        clearInterval(fadeEffect);
+    }
+  }, 200);
+}
+
+
 function displayResults(results) {
 
   var sds_chart = document.getElementById('requested_sds_data');
