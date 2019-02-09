ee868fb268402547dcbf039215578d6e3f8ea432
Aris Stepe <arisstepe@gmail.com>
2019-02-09 18:05:13 -0500

    fixed misnamed html file

--- blueprints/blog/views.py
+++ blueprints/blog/views.py
@@ -6,7 +6,7 @@ from blueprints.blog import blog
 @blog.route('/blog')
 def view_blog():
     posts = BlogPost.query.all()
-    return render_template('blueprints.html', posts=posts)
+    return render_template('blog.html', posts=posts)
 
 
 @blog.route('/blog/<int:id>', methods=['GET'])
